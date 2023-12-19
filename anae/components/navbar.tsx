b0c414b99f94d84260288b5c8d6f9d51ecb15826
Bora <borabalos9@gmail.com>
2023-12-19 17:07:53 +0100

    Home link for nav

--- anae/components/navbar.tsx
+++ anae/components/navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = () => {
     return (
         <div className="border-b">
             <Container>
-                <Link></Link>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                        <p className="font-bold text-xl">Maison Anaé</p>
+                    </Link>
+                </div>
             </Container>
         </div>
     );
