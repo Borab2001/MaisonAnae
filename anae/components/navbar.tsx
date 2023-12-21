3e05430745a54eac24deadbe292bdf120a29dd3e
Bora <borabalos9@gmail.com>
2023-12-21 21:41:20 +0100

    Fetching categories in navbar

--- anae/components/navbar.tsx
+++ anae/components/navbar.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
+import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const categories = await getCategories();
+
     return (
         <div className="border-b">
             <Container>
@@ -11,7 +14,7 @@ const Navbar = () => {
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
                         <p className="font-bold text-xl">Maison Anaé</p>
                     </Link>
-                    <MainNav data={[]}/>
+                    <MainNav data={categories}/>
                 </div>
             </Container>
         </div>
