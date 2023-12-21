4eac487f837338cd2401cd4b06ca18d2e395c78f
Bora <borabalos9@gmail.com>
2023-12-21 21:51:24 +0100

    Styles for unfinished footer

--- anae/components/footer.tsx
+++ anae/components/footer.tsx
@@ -2,14 +2,21 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-black">
-            <div className="mx-auto py-10 text-center">
+        <footer className="bg-black px-4 sm:px-6 lg:px-8">
+            <div className="text-3xl text-white mx-auto py-10 text-center">
                 Maison Anaé
             </div>
             <div className="mx-auto py-10">
-                Footer Sections
+                <p className="text-white uppercase mb-4">Footer Section Titles</p>
+                <ul>
+                    <li>
+                        <a href="#" className="text-sm text-neutral-500 mb-2 hover:text-white transition">
+                            Footer Section list
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <div className="flex flex-row justify-between items-center mx-auto py-10 border-t border-neutral-700">
+            <div className="flex flex-row justify-between items-center mx-auto py-10 border-t border-neutral-700 text-neutral-500">
                 &copy; {currentYear} Maison Anaé
                 <div>
                     Socials here
