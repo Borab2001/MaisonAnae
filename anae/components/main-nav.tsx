3eb584ae80311c762ea5d5251f3191c53176338c
Bora <borabalos9@gmail.com>
2023-12-21 21:26:04 +0100

    data types for category

--- anae/components/main-nav.tsx
+++ anae/components/main-nav.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/libs/utils";
+import { Category } from "@/types";
 
 interface MainNavProps {
-    data: any;
+    data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({
