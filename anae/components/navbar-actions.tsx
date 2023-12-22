6fbeedad7798d59f4154fbe0805789cb65533d87
Bora <borabalos9@gmail.com>
2023-12-22 03:10:31 +0100

    Shopping cart styling

--- anae/components/navbar-actions.tsx
+++ anae/components/navbar-actions.tsx
@@ -6,11 +6,14 @@ import { ShoppingBag } from "lucide-react";
 const NavbarActions = () => {
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex items-center rounded-full bg-black px-4 py-2">
+            <Button className="relative flex items-center rounded-full bg-transparent px-4 py-2">
                 <ShoppingBag 
                     size={20}
-                    color="white"
+                    color="black"
                 />
+                <span className="absolute left-1/2 top-0 w-auto h-4 px-1 py-[2px] flex items-center justify-center text-sm font-medium text-white bg-red-500 rounded-full">
+                    0
+                </span>
             </Button>
         </div>
     );
