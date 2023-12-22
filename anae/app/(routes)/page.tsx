bed164031159239b6fca988d0775ea5b729f4b32
Bora <borabalos9@gmail.com>
2023-12-22 03:22:26 +0100

    Billboard component + billboard type fix

--- anae/app/(routes)/page.tsx
+++ anae/app/(routes)/page.tsx
@@ -1,7 +1,16 @@
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
 const HomePage = () => {
     return (
         <div>
-            Hello Store
+            <Container>
+                <div className="space-y-10 pb-10">
+                    <Billboard />
+                </div>
+            </Container>
         </div>
     );
 }
