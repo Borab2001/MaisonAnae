620dca14fce3ef023a95f4e4345b31fd88af9147
Bora <borabalos9@gmail.com>
2023-12-22 02:39:00 +0100

    Button component

--- anae/components/navbar-actions.tsx
+++ anae/components/navbar-actions.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Button from "@/components/ui/button";
+
 const NavbarActions = () => {
     return (
-        <div>
-            Navbar Actions
+        <div className="ml-auto flex items-center gap-x-4">
+            <Button>
+                
+            </Button>
         </div>
-    )
+    );
 }
 
 export default NavbarActions;
