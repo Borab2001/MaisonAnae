3bf789a00c8d0264fa98748215a3ebb395a021d3
Bora <borabalos9@gmail.com>
2023-12-23 03:11:09 +0100

    test billboard data + next version

--- anae/app/(routes)/page.tsx
+++ anae/app/(routes)/page.tsx
@@ -1,14 +1,18 @@
+import getBillboard from "@/actions/get-billboard";
+
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+    const billboard = await getBillboard("d80a47e7-4abe-4cfe-9110-c48b16d470f0");
+
     return (
         <div>
             <Container>
                 <div className="space-y-10 pb-10">
-                    <Billboard />
+                    <Billboard data={billboard} />
                 </div>
             </Container>
         </div>
