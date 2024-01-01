fe09904de1c8179252feb93cb1e2e8fa5b01555f
Bora <borabalos9@gmail.com>
2024-01-01 17:36:18 +0100

    featured products list in page

--- anae/app/(routes)/page.tsx
+++ anae/app/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
@@ -6,6 +7,7 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products  = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("d80a47e7-4abe-4cfe-9110-c48b16d470f0");
 
     return (
@@ -13,6 +15,9 @@ const HomePage = async () => {
             <Container>
                 <div className="space-y-10 pb-10">
                     <Billboard data={billboard} />
+                </div>
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
                 </div>
             </Container>
         </div>
