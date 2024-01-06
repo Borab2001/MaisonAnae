bfd313243f05626a867d91d7356a8bc8bae16ce9
Bora <borabalos9@gmail.com>
2024-01-06 01:40:45 +0100

    Gallery component

--- anae/app/(routes)/product/[productId]/page.tsx
+++ anae/app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+import Gallery from "@/components/gallery";
 
 interface ProductPageProps {
     params: {
@@ -25,7 +26,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                         {/* Gallery */}
-                        <div>Gallery</div>
+                        <Gallery images={product.images} />
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
                             {/* Product Info */}
                             Info
---
+++ anae/components/gallery/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { Tab } from "@headlessui/react";
+
+import { Image as ImageType } from "@/types";
+
+interface GalleryProps {
+    images: ImageType[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({
+    images
+}) => {
+    return (
+        <Tab.Group as="div" className="flex flex-col-reverse">
+            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
+                    {images.map((image) => (
+                        <GalleryTab key={image.id} image={image} />
+                    ))}
+                </Tab.List>
+            </div>
+        </Tab.Group>
+    );
+}
+ 
+export default Gallery;
