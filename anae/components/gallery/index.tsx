b074577dc6f6763e5d681a7e295ace0dc1039c27
Bora <borabalos9@gmail.com>
2024-01-07 23:31:57 +0100

    Main product gallery image

--- anae/components/gallery/index.tsx
+++ anae/components/gallery/index.tsx
@@ -23,6 +23,20 @@ const Gallery: React.FC<GalleryProps> = ({
                     ))}
                 </Tab.List>
             </div>
+            <Tab.Panels className="aspect-square w-full">
+                {images.map((image) => (
+                    <Tab.Panel key={image.id}>
+                        <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+                            <Image 
+                                fill
+                                src={image.url}
+                                alt="Image"
+                                className="object-cover object-center"
+                            />
+                        </div>
+                    </Tab.Panel>
+                ))}
+            </Tab.Panels>
         </Tab.Group>
     );
 }
