ed613f04827076306311e1397cf8d11eebe4298c
Bora <borabalos9@gmail.com>
2024-01-10 12:37:21 +0100

    Restyling the billboard

--- anae/components/billboard.tsx
+++ anae/components/billboard.tsx
@@ -8,9 +8,9 @@ const Billboard: React.FC<BillboardProps> = ({
     data
 }) => {
     return (
-        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+        <div className="h-[480px] lg:h-[680px] overflow-hidden">
             <div
-                className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+                className="relative overflow-hidden bg-cover top-0 left-0 w-full h-full object-cover"
                 style={{ backgroundImage: `url(${data?.imageUrl})` }}
             >
                 <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
