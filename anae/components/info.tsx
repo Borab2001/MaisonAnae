7d8dd143b469ad745c18358e860484907ba562f3
Bora <borabalos9@gmail.com>
2024-01-10 10:30:29 +0100

    product infos (title, price, size, color)

---
+++ anae/components/info.tsx
@@ -0,0 +1,36 @@
+import { Product } from "@/types";
+import Currency from "./ui/currency";
+
+interface InfoProps {
+    data: Product
+}
+
+const Info: React.FC<InfoProps> = ({
+    data
+}) => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+            <div className="mt-3 flex items-end justify-between">
+                <p className="text-2xl text-gray-900">
+                    <Currency value={data?.price} />
+                </p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex flex-col gap-y-6">
+                <div className="flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Size</h3>
+                    <div>
+                        {data?.size?.value}
+                    </div>
+                </div>
+                <div className="flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Size</h3>
+                    <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Info;
