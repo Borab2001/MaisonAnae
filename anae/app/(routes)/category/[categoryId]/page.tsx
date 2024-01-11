722a7ab8c6e4490f4c3176c915a99d7a7ec32f65
Bora <borabalos9@gmail.com>
2024-01-11 21:34:00 +0100

    categories page styling beginning

--- anae/app/(routes)/category/[categoryId]/page.tsx
+++ anae/app/(routes)/category/[categoryId]/page.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 import getProducts from "@/actions/get-products";
+import getSizes from '@/actions/get-sizes';
+import getColors from '@/actions/get-colors';
+import getCategory from '@/actions/get-category';
+import Container from '@/components/ui/container';
+import Billboard from '@/components/billboard';
 
 export const revalidate = 0;
 
@@ -23,11 +28,31 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         categoryId: params.categoryId,
         colorId: searchParams.colorId,
         sizeId: searchParams.sizeId
-    })
+    });
+
+    const sizes = await getSizes();
+    const colors = await getColors();
+    const category = await getCategory(params.categoryId);
 
     return (
-        <div>
-            Category
+        <div className='bg-white'>
+            <Container>
+                <Billboard
+                    data={category.billboard}
+                />
+                <div className='px-4 sm:px-6 lg:px-8 pb-24'>
+                    <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
+                        {/* Mobile Filters to be developed */}
+                        <div className='hidden lg:block'>
+                            <Filter
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }
