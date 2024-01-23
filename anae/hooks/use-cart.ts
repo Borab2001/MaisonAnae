3f5b9c7ee579c9274bfc6092182fa0cdb3ac07e9
Bora <borabalos9@gmail.com>
2024-01-23 16:07:52 +0100

    Usecart removeAll + storage done

--- anae/hooks/use-cart.ts
+++ anae/hooks/use-cart.ts
@@ -29,8 +29,13 @@ const useCart = create(
         removeItem: (id: string) => {
             set({ items: [...get().items.filter((item) => item.id !== id)] });
             toast.success("Item removed from cart")
-        }
-    }))
+        },
+
+        removeAll: () => set({ items: [] }),
+    }), {
+        name: "cart-storage",
+        storage: createJSONStorage(() => localStorage)
+    })
 )
  
 export default useCart;
