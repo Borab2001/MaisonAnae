024a45ab16a44c1db4ae48d0fadc41937e37178c
Bora <borabalos9@gmail.com>
2024-01-23 16:04:44 +0100

    Use cart hook with addItem done

---
+++ anae/hooks/use-cart.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+import { Product } from "@/types";
+import toast from "react-hot-toast";
+
+interface CartStore {
+    items: Product[];
+    addItem: (data: Product) => void;
+    removeItem: (data: string) => void;
+    removeAll: () => void;
+}
+
+const useCart = create(
+    persist<CartStore>((set, get) => ({
+        items: [],
+        addItem: (data: Product) => {
+            const currentItems = get().items;
+            const existingItem = currentItems.find((item) => item.id === data.id);
+
+            if (existingItem) {
+                return toast("Item already in cart");
+            }
+
+            set({ items: [...get().items, data] });
+            toast.success("Item added to cart");
+        }
+    }))
+)
+ 
+export default useCart;
