bc9b00fbd90277ac9081ee1f8474c66342f71de6
Bora <borabalos9@gmail.com>
2024-01-25 23:31:16 +0100

    Checkout function

--- anae/app/(routes)/cart/components/summary.tsx
+++ anae/app/(routes)/cart/components/summary.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import { Toast } from "react-hot-toast";
+import toast, { Toast } from "react-hot-toast";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
@@ -20,6 +20,25 @@ const Summary = () => {
         return total + Number(item.price)
     }, 0);
 
+    const onCheckout = async () => {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+            productIds: items.map((item) => item.id),
+        });
+
+        window.location = response.data.url;
+    }
+
+    useEffect(() => {
+        if (searchParams.get("successful")) {
+            toast.success("Payment successful");
+            removeAll();
+        }
+
+        if (searchParams.get("cancelled")) {
+            toast.error("Payment unsucessful");
+        }
+    }, [searchParams, removeAll]);
+
     return (
         <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
             <h2 className="text-lg font-medium text-gray-900 uppercase">
@@ -33,7 +52,9 @@ const Summary = () => {
                     <Currency value={totalPrice} locale={""} currency={""} />
                 </div>
             </div>
-            <Button className="w-full mt-6 rounded-md uppercase font-medium">
+            <Button
+                onClick={onCheckout}
+                className="w-full mt-6 rounded-md uppercase font-medium">
                 Checkout
             </Button>
         </div>
