95bf393a76f7433a78adc3eb0ec3831ec643c635
Bora <borabalos9@gmail.com>
2024-01-25 20:13:25 +0100

    More cartItem item styling

--- anae/app/(routes)/cart/components/cart-item.tsx
+++ anae/app/(routes)/cart/components/cart-item.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { Toast } from "react-hot-toast";
-import { X } from "lucide-react";
+import { Trash } from "lucide-react";
 
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
@@ -28,7 +28,30 @@ const CartItem: React.FC<CartItemProps> = ({
             </div>
             <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
                 <div className="absolute z-10 right-0 top-0">
-                    <IconButton onClick={() => {}} icon={<X size={15} />} />
+                    <IconButton 
+                        onClick={() => {}} 
+                        classname="bg-white text-gray-400 border-gray-400 hover:text-red-500 hover:border-red-500 rounded-md shadow-none" 
+                        icon={<Trash size={20} />} 
+                    />
+                </div>
+                <div className="relative pr-9 sm:grid sm:grid-rows-2 sm:gap-x-6 sm:pr-0">
+                    <div className="flex justify-between">
+                        <p className="text-lg font-semibold text-black">
+                            {data.name}
+                        </p>
+                    </div>
+
+                    <div className="mt-1 flex text-sm">
+                        <p className="text-gray-500">
+                            {data.color.name}
+                        </p>
+                        <p className="text-gray-500 ml-4 border-l border-gray-200 pl-4">
+                            {data.size.name}
+                        </p>
+                    </div>
+                    <div className="mt-4">
+                        <Currency value={data.price} locale={""} currency={""} />
+                    </div>
                 </div>
             </div>
         </li>
