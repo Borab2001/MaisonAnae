774f81bc8a60971f2d02e00d7a3cfd225f49ec6b
Bora <borabalos9@gmail.com>
2024-01-30 15:36:25 +0100

    Info height fix

--- anae/components/info.tsx
+++ anae/components/info.tsx
@@ -14,7 +14,7 @@ const Info: React.FC<InfoProps> = ({
     data
 }) => {
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-auto">
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <div className="mt-3 flex items-end justify-between">
                 <p className="text-2xl text-gray-900">
