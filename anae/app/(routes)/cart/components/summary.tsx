9de5647d9b731fc97591d61ec2724af498613391
Bora <borabalos9@gmail.com>
2024-03-14 17:03:03 +0100

    Added loading animation to checkout btn + latest lucide react

--- anae/app/(routes)/cart/components/summary.tsx
+++ anae/app/(routes)/cart/components/summary.tsx
@@ -1,32 +1,22 @@
 "use client";
 
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import toast, { Toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 import CustomButton from "@/components/ui/custom-button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
+import { LoaderCircle } from "lucide-react";
 
 
 const Summary = () => {
-
+    
+    const [loading , setLoading] = useState<boolean>(false);
     const searchParams = useSearchParams(); 
     const items = useCart((state) => state.items);
     const removeAll = useCart((state) => state.removeAll);
-
-    const totalPrice = items.reduce((total, item) => {
-        return total + Number(item.price)
-    }, 0);
-
-    const onCheckout = async () => {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-            productIds: items.map((item) => item.id),
-        });
-
-        window.location = response.data.url;
-    }
 
     useEffect(() => {
         if (searchParams.get("successful")) {
@@ -38,6 +28,19 @@ const Summary = () => {
             toast.error("Payment unsuccessful");
         }
     }, [searchParams, removeAll]);
+
+    const totalPrice = items.reduce((total, item) => {
+        return total + Number(item.price)
+    }, 0);
+
+    const onCheckout = async () => {
+        setLoading(true)
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
+            productIds: items.map((item) => item.id),
+        });
+
+        window.location = response.data.url;
+    }
 
     return (
         <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
@@ -55,11 +58,17 @@ const Summary = () => {
             <CustomButton
                 disabled={items.length === 0}
                 onClick={onCheckout}
-                className="w-full mt-6 rounded-md uppercase font-medium">
-                Checkout
+                className="w-full mt-6 rounded-md uppercase font-medium"
+            >
+                {loading ? (
+                    <div className='flex justify-center cursor-wait space-x-2'>
+                        <LoaderCircle className='w-6 h-6 animate-spin' />
+                        <p>Loading</p>
+                    </div>
+                ) : "Checkout"}
             </CustomButton>
         </div>
     );
 }
- 
+ <LoaderCircle />
 export default Summary;
