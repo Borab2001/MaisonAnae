7d20a6ed6517a05de324768cad6986d2b0846a71
Bora <borabalos9@gmail.com>
2024-03-16 21:17:16 +0100

    Gallery files cleaning

--- anae/components/gallery/index.tsx
+++ anae/components/gallery/index.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import Image from "next/image";
-
-
 import { Image as ImageType } from "@/types";
 
-import GalleryTab from "@/components/gallery/gallery-tab";
 import { Carousel, CarouselContent, CarouselItem, type CarouselApi,  } from "@/components/ui/carousel";
 import React from "react";
 
@@ -18,21 +14,6 @@ const Gallery: React.FC<GalleryProps> = ({
 }) => {
 
     const [api, setApi] = React.useState<CarouselApi>()
-    const [current, setCurrent] = React.useState(0)
-    const [count, setCount] = React.useState(0)
-    
-    React.useEffect(() => {
-        if (!api) {
-        return
-        }
-    
-        setCount(api.scrollSnapList().length)
-        setCurrent(api.selectedScrollSnap() + 1)
-    
-        api.on("select", () => {
-        setCurrent(api.selectedScrollSnap() + 1)
-        })
-    }, [api])
 
     return (
         <Carousel setApi={setApi} orientation="horizontal" className="block h-image-full w-full max-w-full lg:h-screen lg:max-w-[50%] mb-16">
@@ -42,14 +23,7 @@ const Gallery: React.FC<GalleryProps> = ({
                         className="w-full h-full bg-cover bg-center"
                         key={image.id}
                         style={{ backgroundImage: `url(${image.url})` }}
-                    >
-                        {/* <Image 
-                            fill
-                            src={image.url}
-                            alt="Image"
-                            className="object-cover object-center"
-                        /> */}
-                    </CarouselItem>
+                    />
                 ))}
             </CarouselContent>
         </Carousel>
