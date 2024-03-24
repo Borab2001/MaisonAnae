7d06e4a6d4d0b65296589991861dfc8f128f882b
Bora <borabalos9@gmail.com>
2024-03-24 20:59:20 +0100

    Register Modal ui + Label component installed

--- anae/components/modals/register-modal.tsx
+++ anae/components/modals/register-modal.tsx
@@ -8,6 +8,8 @@ import { FcGoogle } from 'react-icons/fc';
 
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 
 const RegisterModal = () => {
@@ -45,7 +47,57 @@ const RegisterModal = () => {
     }
 
     return (
-       <div></div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">Sign Up</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Sign Up</DialogTitle>
+                    <DialogDescription>
+                        Create a new account
+                    </DialogDescription>
+                </DialogHeader>
+
+                <div className="grid gap-4 pt-4">
+                    <div className="flex flex-col items-start gap-2">
+                        <Label htmlFor="name">Full Name</Label>
+                        <Input
+                            id="name"
+                            defaultValue=""
+                            className="col-span-3"
+                            placeholder="John Doe"
+                        />
+
+                        <Label htmlFor="email" className="mt-2">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            defaultValue=""
+                            className="col-span-3"
+                            placeholder="johndoe@email.com"
+                        />
+                        <Label htmlFor="password" className="mt-2">Password</Label>
+                        <Input
+                            id="password"
+                            type="password"
+                            defaultValue=""
+                            className="col-span-3"
+                            placeholder="Password"
+                        />
+
+                        <Label htmlFor="confirmPassword" className="mt-2">Confirm Password</Label>
+                        <Input
+                            id="confirmPassword"
+                            type="password"
+                            defaultValue=""
+                            className="col-span-3"
+                            placeholder="Confirm password"
+                        />
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
     );
 }
  
