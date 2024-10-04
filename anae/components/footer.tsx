5eb486db4fb0be2a22250210dbbde9f6a823654c
Bora <borabalos9@gmail.com>
2024-10-04 17:19:33 +0200

    Refactored and restyled footer

--- anae/components/footer.tsx
+++ anae/components/footer.tsx
@@ -8,105 +8,81 @@ const Footer = () => {
         {
             name: 'Facebook',
             url: 'https://www.facebook.com',
-            icon: <Facebook size={20} />
+            icon: <Facebook size={24} />
         },
         {
             name: 'LinkedIn',
             url: 'https://www.linkedin.com',
-            icon: <Linkedin size={20} />
+            icon: <Linkedin size={24} />
         },
         {
             name: 'Instagram',
             url: 'https://www.instagram.com',
-            icon: <Instagram size={20} />
+            icon: <Instagram size={24} />
         }
     ];
 
-    const commitments = [
+    const footerSections = [
         {
-            name: 'Main Objectives',
-            url: '/main-objectives',
+            title: "Anaé's World",
+            links: [
+                { name: 'Be Circular', url: '/be-circular' },
+                { name: 'Raw Materials', url: '/raw-materials' },
+                { name: 'Certifications', url: '/certifications' },
+            ]
         },
         {
-            name: 'Situation',
-            url: '/situation',
+            title: "Company",
+            links: [
+                { name: 'About Us', url: '/about-us' },
+                { name: 'Main Objectives', url: '/main-objectives' },
+                { name: 'Partners', url: '/partners' },
+            ]
         },
         {
-            name: 'Partners',
-            url: '/partners',
+            title: "Support",
+            links: [
+                { name: 'Privacy Policy', url: '/privacy-policy' },
+                { name: 'Terms of Use', url: '/terms-of-use' },
+                { name: 'Contact Us', url: '/contact-us' },
+            ]
         },
-        {
-            name: 'Be Circular',
-            url: '/be-circular',
-        },
-        {
-            name: 'Raw Materials',
-            url: '/raw-materials',
-        },
-        {
-            name: 'Certifications',
-            url: '/certifications',
-        }
-    ];
+    ]
 
     return (
-        <footer className="bg-gray-50 px-4 sm:px-6 lg:px-8">
-            <div className="text-3xl text-black mx-auto py-10 text-center">
-                Maison Anaé
+        <footer className="bg-primary px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto py-32 grid grid-cols-2 lg:grid-cols-5 gap-8 w-full justify-between">
+                <div className="col-span-2 text-3xl text-white flex flex-col items-start">
+                    Maison Anaé
+                    <p className="text-sm text-neutral-300 mt-2">Sustainable Parisian Fashion</p>
+                    <div className="flex flex-row justify-between items-center gap-x-4 my-6">
+                        {socialLinks.map((link, index) => (
+                            <Link
+                                key={index}
+                                href={link.url}
+                                className="text-neutral-300 hover:bg-neutral-800 p-1 rounded-md transition"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label={link.name}
+                            >
+                                {link.icon}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                {footerSections.map((section, sectionIndex) => (
+                    <div key={sectionIndex} className="w-fit flex flex-col gap-y-2">
+                        <p className="w-max text-white font-medium uppercase mb-4">{section.title}</p>
+                        {section.links.map((link, linkIndex) => (
+                            <Link key={linkIndex} href={link.url} className="w-fit text-sm text-neutral-400 mb-2 hover:text-neutral-200 transition">
+                                {link.name}
+                            </Link>
+                        ))}
+                    </div>
+                ))}
             </div>
-            <div className="mx-auto py-10 grid grid-cols-2 lg:grid-cols-4 gap-8 w-full justify-between">
-                <div className="w-fit flex flex-col gap-y-2">
-                    <p className="w-max text-black font-medium uppercase mb-4">About Us</p>
-                    {commitments.map((commitment, index) => (
-                        <Link key={index} href={commitment.url} className="w-fit text-sm uppercase text-neutral-500 mb-2 hover:text-black transition">
-                            {commitment.name}
-                        </Link>
-                    ))}
-                </div>
-
-                <div className="w-fit flex flex-col gap-y-2">
-                    <p className="w-max text-black font-medium uppercase mb-4">Anaé's World</p>
-                    {commitments.map((commitment, index) => (
-                        <Link key={index} href={commitment.url} className="w-fit text-sm uppercase text-neutral-500 mb-2 hover:text-black transition">
-                            {commitment.name}
-                        </Link>
-                    ))}
-                </div>
-
-                <div className="w-fit flex flex-col gap-y-2">
-                    <p className="w-max text-black font-medium uppercase mb-4">Collections</p>
-                    {commitments.map((commitment, index) => (
-                        <Link key={index} href={commitment.url} className="w-fit text-sm uppercase text-neutral-500 mb-2 hover:text-black transition">
-                            {commitment.name}
-                        </Link>
-                    ))}
-                </div>
-
-                <div className="w-fit flex flex-col gap-y-2">
-                    <p className="w-max text-black font-medium uppercase mb-4">Our Commitments</p>
-                    {commitments.map((commitment, index) => (
-                        <Link key={index} href={commitment.url} className="w-fit text-sm uppercase text-neutral-500 mb-2 hover:text-black transition">
-                            {commitment.name}
-                        </Link>
-                    ))}
-                </div>
-            </div>
-            <div className="flex flex-row justify-between items-center mx-auto py-10 border-t border-neutral-700 text-neutral-500">
-                &copy; {currentYear} Maison Anaé
-                <div className="flex flex-row justify-between items-center">
-                    {socialLinks.map((link, index) => (
-                        <Link
-                            key={index}
-                            href={link.url}
-                            className="text-xl mx-2 hover:text-black transition"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label={link.name}
-                        >
-                            {link.icon}
-                        </Link>
-                    ))}
-                </div>
+            <div className="flex flex-row text-sm justify-center items-center mx-auto py-10 border-t border-neutral-700 text-neutral-300">
+                &copy; Groupe BNSB {currentYear}. All rights reserved.
             </div>
         </footer>
     );
